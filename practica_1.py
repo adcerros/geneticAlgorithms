90cead9c64912b54348096ae5514818bc284120b
ADRIÁN CERROS SÁNCHEZ <100405342@alumnos.uc3m.es>
2022-09-21 01:43:43 +0200

    VERSION ESTABLE

--- practica_1.py
+++ practica_1.py
@@ -3,6 +3,9 @@ import requests
 from random import randint
 
 mutation_factor = 0.05
+POBLATION_SIZE = 1000
+CRHOMOSOME_SIZE = 32
+ROUNDS = 20
 url = "http://memento.evannai.inf.uc3m.es/age/test?c="
 
 def my_call(url, chromosome):
@@ -15,19 +18,15 @@ def my_call(url, chromosome):
     return my_request.text
 
 def evaluate(poblation):
-    fitness_matrix = []
-    for i in range(len(poblation)):
-        fitness_matrix.append(float(my_call(url, poblation[i])))
-    return fitness_matrix
+    return [float(my_call(url, poblation[i])) for i in range(len(poblation))]
 
 
 def create_initial():
-    poblation = [[ str(randint(0,1)) for x in range(32)] for x in range (100)]
-    return poblation
+    return [[str(randint(0,1)) for x in range(CRHOMOSOME_SIZE)] for x in range (POBLATION_SIZE)]
 
 def tournament(poblation, fitness_matrix):
     winners = []
-    for i in range(len(fitness_matrix)):
+    for i in range(POBLATION_SIZE):
         round_competitors = []
         for j in range(4):
             pos = randint(0,99)
@@ -37,9 +36,9 @@ def tournament(poblation, fitness_matrix):
 
 def mix(winners):
     new_poblation = []
-    for i in range(len(winners) - 1):
+    for i in range(0, POBLATION_SIZE, 2):
         first_son, second_son = "", ""
-        for j in range(len(winners[i])):
+        for j in range(CRHOMOSOME_SIZE):
             first_son += winners[i + randint(0,1)][j]
             second_son += winners[i + randint(0,1)][j]
         new_poblation.append(first_son)
@@ -48,12 +47,13 @@ def mix(winners):
     
 def make_generation(poblation):
     fitness_matrix = evaluate(poblation)
+    print("Mejor resultado: ", max(fitness_matrix))
     return mix(tournament(poblation, fitness_matrix))
 
 
 
 poblation = create_initial()
-for i in range(25):
+for i in range(ROUNDS):
     print("Realizando generacion " + str(i) + " ...")
     poblation = make_generation(poblation)
 fitness_matrix = evaluate(poblation)
