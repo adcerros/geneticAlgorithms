798a9e728083d2ae063e44f6fe5366f8f91eec6c
ADRIÁN CERROS SÁNCHEZ <100405342@alumnos.uc3m.es>
2022-09-24 03:29:37 +0200

    segunda version finalizada

--- practica_1.py
+++ practica_1.py
@@ -5,7 +5,8 @@ from random import randint
 MUTATION_FACTOR = 15
 POBLATION_SIZE = 100
 CRHOMOSOME_SIZE = 32
-ROUNDS = 4000
+ROUNDS = 300
+REPLACE_SIZE = 10
 url = "http://memento.evannai.inf.uc3m.es/age/test?c="
 
 
@@ -35,36 +36,56 @@ def tournament(poblation, fitness_matrix):
         winners.append(poblation[round_competitors[round_competitors.index(max(round_competitors))][1]])
     return winners
 
+def sort_by_fitness(poblation, fitness_matrix):
+    return [x for _, x in sorted(zip(fitness_matrix, poblation), reverse=True)]
+
+def mix_and_replace(poblation, fitness_matrix):
+    new_poblation = sort_by_fitness(poblation, fitness_matrix)
+    for i in range(REPLACE_SIZE):  
+        new_poblation[-i], new_poblation[-(i+1)] = get_sons(new_poblation[i], new_poblation[i+1]) 
+    return new_poblation
+
+
+
+
 def mix(winners):
     new_poblation = []
     for i in range(0, POBLATION_SIZE, 2):
-        first_son, second_son = [], []
-        for j in range(CRHOMOSOME_SIZE):
-            first_son.append(winners[i + randint(0,1)][j])
-            if randint(0,100) < MUTATION_FACTOR:
-                first_son[-1] = '0' if first_son[-1] == '1' else '1'
-            second_son.append(winners[i + randint(0,1)][j])
-            if randint(0,100) < MUTATION_FACTOR:
-                second_son[-1] = '0' if second_son[-1] == '1' else '1' 
+        first_son, second_son = get_sons(winners[i], winners[i+1]) 
         new_poblation.append(first_son)
         new_poblation.append(second_son)
     return new_poblation
+
+def get_sons(first_parent, second_parent):
+    first_son, second_son = [], []
+    for j in range(CRHOMOSOME_SIZE):
+        first_son.append(first_parent[j] if randint(0,1) == 0 else second_parent[j])
+        if randint(0,100) < MUTATION_FACTOR:
+            first_son[-1] = 0 if first_son[-1] == 1 else 1
+        second_son.append(first_parent[j] if randint(0,1) == 0 else second_parent[j])
+        if randint(0,100) < MUTATION_FACTOR:
+            second_son[-1] = 0 if second_son[-1] == 1 else 1
+    return first_son, second_son
     
 def make_generation(poblation):
     fitness_matrix = evaluate(poblation)
-    return mix(tournament(poblation, fitness_matrix)), max(fitness_matrix)
+    # Utilizando torneos
+    return mix(tournament(poblation, fitness_matrix)), max(fitness_matrix), int(sum(fitness_matrix) / POBLATION_SIZE)
+    # Sustituyendo a los 10 peores en cada ciclo
+    # return mix_and_replace(poblation, fitness_matrix), max(fitness_matrix), int(sum(fitness_matrix) / POBLATION_SIZE)
+    # Haciendo que los mejores padres tengan mas probabilidad de tener hijos en el torneo (siendo escogidos)
 
 def run():
     best_fitness = []
     poblation = create_initial()
     for i in range(ROUNDS):
         print("Realizando generacion", i , "...")
-        poblation, best = make_generation(poblation)
-        print("Mejor resultado: ", best)
-        best_fitness.append(best)
-        if i > 10 and abs(best_fitness[-1] - best_fitness[-10]) < 50:
-            return max(best_fitness)
+        poblation, best, fitness_mean = make_generation(poblation)
+        print("Mejor resultado: ", best, "Fitness medio:", fitness_mean)
+        # best_fitness.append(best)
+        # if i > 10 and abs(best_fitness[-1] - best_fitness[-10]) < 50:
+        #     return max(best_fitness)
 
 
-print("\El mejor resultado obtenido ha sido:", run(), "\n")
+print("\nEl mejor resultado obtenido ha sido:", run(), "\n")
 
