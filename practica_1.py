2501cee47406421cf90baf74a7e4ce3e6a1142a9
adCerros <adriancerrossanchez@gmail.com>
2022-10-03 20:59:17 +0200

    version estable en decimal

--- practica_1.py
+++ practica_1.py
@@ -1,6 +1,6 @@
 from time import sleep
 import requests
-from random import randint
+from random import randint, gauss
 
 
 
@@ -9,8 +9,9 @@ url = "http://163.117.164.219/age/test?c="
 
 
 def my_call(url, chromosome):
+    binary_cromosome = "".join(["{0:08b}".format(gen) for gen in chromosome])
     try:
-        my_request = requests.get(url + ''.join(str(x) for x in chromosome))
+        my_request = requests.get(url + binary_cromosome)
     except:
         print("Intentando reconectar ...")
         sleep(0.2)
@@ -18,11 +19,11 @@ def my_call(url, chromosome):
     return my_request.text
 
 def evaluate(poblation):
-    return [float(my_call(url, poblation[i])) for i in range(len(poblation))]
+    return [float(my_call(url, elem)) for elem in poblation]
 
 
-def create_initial(poblations_size, gens_number, gen_size):
-    return [[randint(0,1) for x in range(gens_number * gen_size)] for x in range (poblations_size)]
+def create_initial(poblations_size, gens_number):
+    return [[randint(0,255) for _ in range(gens_number)] for _ in range(poblations_size)]
 
 def tournament(poblation, fitness_matrix, tournament_size = 4):
     winners = []
@@ -45,34 +46,33 @@ def replace(poblation, fitness_matrix, replace_size):
         new_poblation[-i] = new_poblation[i] 
     return new_poblation
 
-def mix(winners, mutation_factor, gens_number, gen_size):
+def mix(winners, mutation_factor, gens_number):
     new_poblation = []
     for i in range(0, len(winners), 2):
-        first_son, second_son = get_sons(winners[i], winners[i+1], gens_number, gen_size) 
-        new_poblation.append(mutation(first_son, gens_number, gen_size, mutation_factor))
-        new_poblation.append(mutation(second_son, gens_number, gen_size, mutation_factor))
+        first_son, second_son = get_sons(winners[i], winners[i+1], gens_number) 
+        new_poblation.append(mutation(first_son, gens_number, mutation_factor))
+        new_poblation.append(mutation(second_son, gens_number, mutation_factor))
     return new_poblation
 
 
 # Se toma el cromosoma entero de uno de los progenitores
-def get_sons(first_parent, second_parent, gens_number, gen_size):
+def get_sons(first_parent, second_parent, gens_number):
     first_son, second_son = [], []
-    for j in range(0, gens_number * gen_size, gen_size):
-        first_son += first_parent[j : j + gen_size] if randint(0,1) == 0 else second_parent[j : j + gen_size]
-        second_son += first_parent[j : j + gen_size] if randint(0,1) == 0 else second_parent[j : j + gen_size]
+    for i in range(gens_number):
+        first_son.append(first_parent[i]) if randint(0,1) == 0 else first_son.append(second_parent[i])
+        second_son.append(first_parent[i]) if randint(0,1) == 0 else second_son.append(second_parent[i])
     return first_son, second_son
 
 
-def mutation(son, gens_number, gen_size, mutation_factor):
+def mutation(son, gens_number, mutation_factor, mean=0, standard_derivation=30):
     if randint(0,100) < mutation_factor:
-        pos = randint(0, gens_number - 1) * 8
-        gen_to_change = int(''.join(str(x) for x in son[pos:pos + gen_size]), 2)
-        new_gen = gen_to_change + randint(-5,5)
+        pos = randint(0, gens_number - 1)
+        new_gen = son[pos] + int(gauss(mean, standard_derivation))
         if new_gen < 0:
             new_gen = 0
         elif new_gen > 255:
             new_gen = 255
-        son = son[:pos] + [int(x) for x in "{0:08b}".format(int(new_gen))] + son[pos + gen_size:]
+        son[pos] = new_gen
     return son
 
 
@@ -82,34 +82,37 @@ def clone(best_ones, poblation):
     return poblation
 
 
-def make_generation(poblation, mutation_factor, gens_number, gen_size, replace_size, clone_size):
+def make_generation(poblation, mutation_factor, gens_number, replace_size, clone_size):
     fitness_matrix = evaluate(poblation)
     sorted_and_replaced_poblation = replace(poblation, fitness_matrix, replace_size)
     winners = tournament(sorted_and_replaced_poblation, fitness_matrix)
-    new_poblation = mix(winners, mutation_factor, gens_number, gen_size)
+    new_poblation = mix(winners, mutation_factor, gens_number)
     new_poblation = clone(sorted_and_replaced_poblation[:clone_size], new_poblation)
     return new_poblation, min(fitness_matrix)
 
 
 
-def run(poblations_size = 1000, rounds=500, mutation_factor=5, gens_number=10, gen_size=8, replace_size=5, clone_size=2):
+def run(poblations_size = 100, rounds=1000, mutation_factor=25, gens_number=10, replace_size=5, clone_size=1):
     #best_ones_list = []
-    poblation = create_initial(poblations_size, gens_number, gen_size)
+    poblation = create_initial(poblations_size, gens_number)
     for i in range(rounds):
         print("Realizando generacion", i , "...")
-        poblation, best = make_generation(poblation, mutation_factor, gens_number, gen_size, replace_size, clone_size)
-        print("Mejor valor de la generacion:", best)
+        poblation, best_value = make_generation(poblation, mutation_factor, gens_number, replace_size, clone_size)
+        print("Mejor valor de la generacion:", best_value)
         #best_ones_list.append(best)
         # if i > 10 and abs(best_ones_list[-10] - best_ones_list[0]) < 0.10:
         #     print("Se ha alcanzado un minimo local")
         #     return min(best_ones_list)
-        if best == 0:
+        if best_value == 0:
             print("Se ha alcanzado el resultado optimo")
-            return best
+            fitness_matrix = evaluate(poblation)
+            poblation = sort_by_fitness(poblation, fitness_matrix)
+            print("La solucion optima es:", poblation[0])
+            return best_value
 
 
 
-print("\nEl mejor resultado obtenido ha sido:", run(), "\n")
+run()
 #OPCIONES
 # Coger la mejor parte de cada padre evaluandolos por partes
 # juntar a los mejores padres
