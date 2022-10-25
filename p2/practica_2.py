242e82511a0160d8d035fffa3995781060c067e4
adCerros <adriancerrossanchez@gmail.com>
2022-10-25 21:38:42 +0200

    version1 finalizada

--- p2/practica_2.py
+++ p2/practica_2.py
@@ -31,13 +31,17 @@ def evaluate(subject):
 
 def create_initial(poblations_size, gens_number):
     initial_poblation = [[uniform(-180,180) for _ in range(gens_number)] for _ in range(poblations_size)]
-    return initial_poblation, [1.0 for _ in range(poblations_size)], [evaluate(subject) for subject in initial_poblation], [[] for _ in range(poblations_size)]
+    return initial_poblation, [1000.0 for _ in range(poblations_size)], [evaluate(subject) for subject in initial_poblation], [[] for _ in range(poblations_size)]
 
 
 
 #DIVERSIDAD GENETICA!!!!!!!!!!
 # genetic_diversity = statistics.mean([statistics.pstdev([elem[column] for elem in poblation]) for column in range(gens_number)])
 
+
+#Version que toma la ventana en cada iteraccion
+
+#V_1
 def variances_mutation(subject_variance, subject_last_results, c=0.817, window_size=10):
     if len(subject_last_results) < window_size:
         return subject_variance, subject_last_results
@@ -49,8 +53,17 @@ def variances_mutation(subject_variance, subject_last_results, c=0.817, window_s
     elif hit_rate > 0.2:
         return subject_variance / c, subject_last_results
     return subject_variance, subject_last_results
-        
 
+# V_2     
+# def variances_mutation(subject_variance, subject_last_results, c=0.817, window_size=10):
+#     if len(subject_last_results) != window_size:
+#         return subject_variance, subject_last_results
+#     hit_rate = subject_last_results.count(1) / window_size
+#     if hit_rate < 0.2:
+#         return c * subject_variance, []
+#     elif hit_rate > 0.2:
+#         return subject_variance / c, []
+#     return subject_variance, []
 
 
 
@@ -78,7 +91,7 @@ def make_generation(poblation, pob_variances, fitness_matrix, last_results):
     return new_poblation, new_pob_variances, new_fitness_matrix, new_last_results
 
 
-def run(poblations_size=1, rounds=200, gens_number=10):
+def run(poblations_size=1, rounds=200, gens_number=4):
     start_time = time.time()
     poblation, pob_variances, fitness_matrix, last_results = create_initial(poblations_size, gens_number)
     data_file = open(str(poblations_size) + "_pob_" +  str(rounds) +  "_runs_.txt", "w+")
@@ -87,20 +100,21 @@ def run(poblations_size=1, rounds=200, gens_number=10):
             print("\nRealizando generacion", i + 1)
             poblation, pob_variances, fitness_matrix, last_results = make_generation(poblation, pob_variances, fitness_matrix, last_results)
             best_one_fitness = min(fitness_matrix)
-            best_one = poblation[fitness_matrix.index(best_one_fitness)]
-            print("Mejor fitness", best_one_fitness)
-            print("Mejor individuo", best_one)
-            data_file.write(str(best_one_fitness) + "\n")
-            if best_one_fitness == 0:
+            genetic_diversity = statistics.mean([statistics.pstdev([elem[column] for elem in poblation]) for column in range(gens_number)])
+            print("Mejor fitness", best_one_fitness, "Diversidad genetica", genetic_diversity)
+            data_file.write(str(best_one_fitness) + "," + str(genetic_diversity) + "\n")
+            if best_one_fitness <= 1.0e-06:
                 data_file.close()
                 print("Finalizado con:", poblations_size, "poblacion//", i, "rondas//", poblations_size * i, "llamadas al sistema//", round((time.time() - start_time) / 60, 2), "min de tiempo transcurrido")
-                print("La solucion encontrada es:", best_one, "con un valor de fitness", best_one_fitness)
+                best_one = poblation[fitness_matrix.index(best_one_fitness)]
+                best_one_variance = pob_variances[fitness_matrix.index(best_one_fitness)]
+                print("La solucion encontrada es:", best_one, "con un valor de fitness", best_one_fitness, "Varianzas", best_one_variance)
                 return
         data_file.close()
-        print("Finalizado con:", poblations_size, "poblacion//", rounds, "rondas//", poblations_size * rounds, "llamadas al sistema//", round((time.time() - start_time) / 60, 2), "min de tiempo transcurrido")
-    except:
+        print("Finalizado normal con:", poblations_size, "poblacion//", rounds, "rondas//", poblations_size * rounds, "llamadas al sistema//", round((time.time() - start_time) / 60, 2), "min de tiempo transcurrido")
+    except Exception as e:
         data_file.close()
-        print("Finalizado con:", poblations_size, "poblacion//", rounds, "rondas//", poblations_size * rounds, "llamadas al sistema//", round((time.time() - start_time) / 60, 2), "min de tiempo transcurrido")
+        print("Excepcion con:", poblations_size, "poblacion//", rounds, "rondas//", poblations_size * rounds, "llamadas al sistema//", round((time.time() - start_time) / 60, 2), "min de tiempo transcurrido")
 
 
 def collect_data():
@@ -135,7 +149,7 @@ def run_multiple(params):
 # COMENTAR SI SE DESEA UNICAMENTE GENERAR LAS GRAFICAS
 # params = poblation_size, rounds, gens_number
 run_multiple([ 
-[100, 5000, 4]
+[2000, 100000, 4]
 ])
 
 # /////////////////////
